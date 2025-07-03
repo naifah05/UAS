78c753f71cafde91efcec0a857cad4f6f0e797bd
djambred <djambred@gmail.com>
2025-07-03 15:28:45 +0700

    added encrypted

--- template/src/routes/web.php
+++ template/src/routes/web.php
@@ -2,10 +2,12 @@
 
 use Illuminate\Support\Facades\Route;
 use Livewire\Livewire;
+use Illuminate\Support\Facades\Response;
 
 /* NOTE: Do Not Remove
 / Livewire asset handling if using sub folder in domain
 */
+
 Livewire::setUpdateRoute(function ($handle) {
     return Route::post(config('app.asset_prefix') . '/livewire/update', $handle);
 });
